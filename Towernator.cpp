32cf4f99a958bae4b24c7e109323e8006b0c31ed
Adomas <adomas@stalas.lt>
2025-03-30 20:46:26 +0300

    added imgui

--- Towernator.cpp
+++ Towernator.cpp
@@ -2,38 +2,51 @@
 #include <SFML/Graphics.hpp>
 #include "GameObjects.hpp"
 
+#include "imgui.h"
+#include "imgui-SFML.h"
 
 
 int main()
 {
-	sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode(sf::Vector2u(700, 700)), "Testing");
+	sf::RenderWindow window(sf::VideoMode(sf::Vector2u(800, 800)), "Test window");
+	ImGui::SFML::Init(window);
+
+	sf::CircleShape shape(200, 100);
+	shape.setFillColor(sf::Color(0, 255, 255));
+	shape.setPosition(sf::Vector2f(200, 200)); // Center circle
 
 	GameObjects::Game& game = GameObjects::Game::getInstance();
 
 	game.initialize(GameObjects::Difficulty::MEDIUM);
 
-	while (window->isOpen())
+	sf::Clock deltaClock;
+	while (window.isOpen())
 	{
-		while (const std::optional event = window->pollEvent())
+		const sf::Event UIevent();
+		while (const std::optional event = window.pollEvent())
 		{
+			ImGui::SFML::ProcessEvent(window, *event);
 			if (event->is<sf::Event::Closed>())
 			{
-				window->close();
+				window.close();
 			}
 		}
+		ImGui::SFML::Update(window, deltaClock.restart());
+
+		ImGui::Begin("Window title");
+		ImGui::Text("Window text");
+		ImGui::End();
 
 		//main loop will go here
 		//should primarily be used for high level function calls
 
 		//render 
-
-		window->clear();
-
-		//draw stuff
-
-		window->display();
+		window.clear(sf::Color(18, 33, 43)); // Color background
+		window.draw(shape);
+		ImGui::SFML::Render(window);
+		window.display();
 	}
 
-	delete window;
+	ImGui::SFML::Shutdown();
 	return 0;
 }
